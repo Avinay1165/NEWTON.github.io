68528ec4d206117510a41334a84dc42a403da97d
Avinay Mehta <avinay1165@gmail.com>
2021-01-29 18:31:12 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+
+
+ReactDOM.render(<><p> I am learning React. My life is getting better.</p> </>, document.getElementById("root"));
